209886e86be2c46cc6ea0017ee09649624430e6c
Poizo <poizo.artist@gmail.com>
2023-12-03 17:51:37 +0100

    build styles in css files

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,64 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "production",
-  entry: path.resolve(__dirname, "./src/index.ts"),
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
+module.exports = [
+  {
+    mode: "production",
+    entry: path.resolve(__dirname, "./src/index.ts"),
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+      },
+      ],
     },
-    {
-        test: /\.(sa|sc|c)ss$/,
-        exclude: /node_modules/,
-        use: [
-            "style-loader",
-            'css-loader',
-            "postcss-loader",
-            "sass-loader",
-        ],
+    resolve: {
+      extensions: [ ".ts", ".js"],
     },
+    output: {
+      filename: "script.js",
+      path: path.resolve(__dirname, "public", "bundle"),
+    },
+  },
+  {
+    mode: "production",
+    entry: path.resolve(__dirname, "./src/styles.js"),
+    module: {
+      rules: [
+        {
+            test: /\.(sa|sc|c)ss$/,
+            exclude: /node_modules/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                "postcss-loader",
+                "sass-loader",
+            ],
+        },
+      ],
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          styles: {
+            name: "styles",
+            type: "css/mini-extract",
+            chunks: "all",
+            enforce: true,
+          },
+        },
+      },
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "styles.css",
+        chunkFilename: "styles.css"
+      }),
     ],
-  },
-  resolve: {
-    extensions: [ ".ts", ".js"],
-  },
-  output: {
-    filename: "script.js",
-    path: path.resolve(__dirname, "public", "bundle"),
-  },
-};
+    output: {
+      path: path.resolve(__dirname, "public", "bundle"),
+    },
+  }
+];
